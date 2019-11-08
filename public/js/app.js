90c1f1980eb2fd94488a5e79fb27dc106642e731
tausolar <gustavockra@gmail.com>
2019-11-08 14:23:42 -0300

    Removed unnecessary comments on app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,5 @@
 console.log('client side javascript file is loaded')
 
-// fetch('http://localhost:3000/weather?address='+ location).then((response) => {
-//     response.json().then((data) =>{
-    
-//         if (data.error){
-//             console.log(data.error)
-//         } else{
-
-//        console.log(data.location)
-        
-
-//         console.log(data.forecast)
-
-//         }
-
-//     })
-
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageONe = document.querySelector('#message-1')
